908be550c4e2eaac6331702619ac25d7a3a6d116
Sycrusian <sycrus@protonmail.com>
2024-11-13 11:00:20 -0300

    feat:Added server-side rendering of pokemon listing from PokeAPI

---
+++ src/app/pokemons/page.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+
+export interface PokemonData {
+  name: string;
+  url: string;
+  sprites: {
+    front_default: string;
+  }
+}
+
+export interface PokemonCollection {
+  results: PokemonData[]
+}
+
+const catchPokemons = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+  const pokemons: PokemonCollection = await response.json();
+  return pokemons;
+}
+
+const Pokemons = async () => {
+  const pokemons = await catchPokemons();
+  return (
+    <ul>
+      {pokemons.results.map((pokemon, index) => {
+        return (
+          <Link key={index} href={`/pokemon/${index+1}`}>
+            <li>{pokemon.name}</li>
+          </Link>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default Pokemons;
